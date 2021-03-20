9f5f63761f50079b8232731321d12eab71afeb0e
“Daniel <danielnottingham.ti@gmail.com>
2021-03-20 12:53:34 -0300

    validates and generate coupons

--- test/system/promotions_test.rb
+++ test/system/promotions_test.rb
@@ -2,6 +2,7 @@ require 'application_system_test_case'
 
 class PromotionsTest < ApplicationSystemTestCase
   test 'view promotions' do
+    # arrange
     Promotion.create!(name: 'Natal', description: 'Promoção de Natal',
                       code: 'NATAL10', discount_rate: 10, coupon_quantity: 100,
                       expiration_date: '22/12/2033')
@@ -10,9 +11,11 @@ class PromotionsTest < ApplicationSystemTestCase
                       code: 'CYBER15', discount_rate: 15,
                       expiration_date: '22/12/2033')
 
+    # act
     visit root_path
     click_on 'Promoções'
 
+    # assert
     assert_text 'Natal'
     assert_text 'Promoção de Natal'
     assert_text '10,00%'
@@ -72,5 +75,70 @@ class PromotionsTest < ApplicationSystemTestCase
     click_on 'Voltar'
 
     assert_current_path promotions_path
+  end
+
+  test 'create promotion' do
+    visit root_path
+    click_on 'Promoções'
+    click_on 'Registrar uma promoção'
+    fill_in 'Nome', with: 'Cyber Monday'
+    fill_in 'Descrição', with: 'Promoção de Cyber Monday'
+    fill_in 'Código', with: 'CYBER15'
+    fill_in 'Desconto', with: '15'
+    fill_in 'Quantidade de cupons', with: '90'
+    fill_in 'Data de término', with: '22/12/2033'
+    click_on 'Criar promoção'
+
+    # assert_current_path promotion_path(Promotion.last)
+    assert_text 'Cyber Monday'
+    assert_text 'Promoção de Cyber Monday'
+    assert_text '15,00%'
+    assert_text 'CYBER15'
+    assert_text '22/12/2033'
+    assert_text '90'
+    assert_link 'Voltar'
+  end
+
+  test 'create and attributes cannot be blank' do
+    visit root_path
+    click_on 'Promoções'
+    click_on 'Registrar uma promoção'
+    click_on 'Criar promoção'
+
+    assert_text 'não pode ficar em branco', count: 5
+  end
+
+  test 'create and code/name must be unique' do
+    Promotion.create!(name: 'Natal', description: 'Promoção de Natal',
+                      code: 'NATAL10', discount_rate: 10, coupon_quantity: 100,
+                      expiration_date: '22/12/2033')
+
+    visit root_path
+    click_on 'Promoções'
+    click_on 'Registrar uma promoção'
+    fill_in 'Nome', with: 'Natal'
+    fill_in 'Código', with: 'NATAL10'
+    click_on 'Criar promoção'
+
+    assert_text 'deve ser único', count: 2
+  end
+
+  test 'generate coupons for a promotion' do
+    promotion = Promotion.create!(name: 'Natal',
+                                  description: 'Promoção de Natal',
+                                  code: 'NATAL10', discount_rate: 10,
+                                  coupon_quantity: 100,
+                                  expiration_date: '22/12/2033')
+
+    visit promotion_path(promotion)
+    click_on 'Gerar cupons'
+
+    assert_text 'Cupons gerados com sucesso'
+    assert_no_link 'Gerar cupons'
+    assert_no_text 'NATAL10-0000'
+    assert_text 'NATAL10-0001'
+    assert_text 'NATAL10-0002'
+    assert_text 'NATAL10-0100'
+    assert_no_text 'NATAL10-0101'
   end
 end
